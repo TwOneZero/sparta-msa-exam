a1ca88f283bfab1ad2bbf966f562ea1d66ddd55e
twonezero <dnjsdudtm98@naver.com>
2024-08-09 22:30:01 +0900

    Auth - fix : http 요청 어노테이션 수정 (get)

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
@@ -4,7 +4,7 @@ package com.sparta.msa_exam.auth;
 import com.sparta.msa_exam.auth.dto.AuthResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +14,7 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @PostMapping("/auth/signIn")
+    @GetMapping("/auth/signIn")
     public ResponseEntity<AuthResponse> signIn(
             @RequestParam(name = "user_id") String userId
     ) {
